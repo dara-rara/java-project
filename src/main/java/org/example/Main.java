bdfb3aa89136a92a2b80175349ef43f6252ed68c
dara-rara <glebova555dasha@gmail.com>
2023-11-12 00:40:54 +0500

    add

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,8 @@
 package org.example;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Main {
     public static void main(String[] args) {
         ParserCSV parser = new ParserCSV();
@@ -8,6 +12,11 @@ public class Main {
         parser.createMapExercisesAndHomeworks();
         var students = parser.createStudentStorage();
         var student = students.getStudent("Новикова Мария");
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(student.toString());
     }
 }
