b883d4a80ca8ae56b596f3e8e9ab89d106dab2a6
dara-rara <glebova555dasha@gmail.com>
2023-11-11 23:13:12 +0500

    add

---
+++ src/main/java/org/example/ParserCSV.java
@@ -0,0 +1,109 @@
+package org.example;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+
+public class ParserCSV {
+    private List<String[]> table;
+    private HashMap<String, Integer> mapTheme;
+    private HashMap<String, Integer> mapSeminar;
+    private HashMap<String, HashMap<String, Integer>> mapExercises;
+    private HashMap<String, HashMap<String, Integer>> mapHomeworks;
+
+    public void readFile(String file) {
+        CSVParser csvParser = new CSVParserBuilder().withSeparator(';').build();
+        try (CSVReader reader = new CSVReaderBuilder(
+                new InputStreamReader(new FileInputStream(file),"UTF-8"))
+                .withCSVParser(csvParser)
+                .build()) {
+            table = reader.readAll();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    public void createMapTheme() {
+        var firstLine = table.get(0);
+        mapTheme = new HashMap<>();
+        for (var i = 8; i < firstLine.length; i++) {
+            if (firstLine[i] != null && firstLine[i] != "") {
+                mapTheme.put(firstLine[i], i);
+            }
+        }
+    }
+
+    public void createMapSeminar() {
+        var secondLine = table.get(1);
+        mapSeminar = new HashMap<>();
+        for (var nameTheme : mapTheme.entrySet()) {
+            for (var i = nameTheme.getValue(); i < secondLine.length; i++) {
+                if (secondLine[i].equals("Сем")) {
+                    mapSeminar.put(nameTheme.getKey(), i);
+                    break;
+                }
+            }
+        }
+    }
+
+    public void createMapExercisesAndHomeworks() {
+        mapExercises = new HashMap<>();
+        mapHomeworks = new HashMap<>();
+        var secondLine = table.get(1);
+        for (var nameTheme : mapTheme.entrySet()) {
+            mapExercises.put(nameTheme.getKey(), new HashMap<>());
+            mapHomeworks.put(nameTheme.getKey(), new HashMap<>());
+            for (var i = nameTheme.getValue(); i < secondLine.length; i++) {
+                if (secondLine[i].equals("Сем")) {
+                    break;
+                }
+                if (secondLine[i].contains("Упр:")) {
+                    var temp = mapExercises.get(nameTheme.getKey());
+                    temp.put(secondLine[i], i);
+                }
+                if (secondLine[i].contains("ДЗ:")) {
+                    var temp = mapHomeworks.get(nameTheme.getKey());
+                    temp.put(secondLine[i], i);
+                }
+            }
+        }
+    }
+
+    public Student createStudent(String[] data) {
+        var student = new Student(data[0], data[1]);
+        for (var nameTheme : mapTheme.entrySet()) {
+            var themeExercises = new HashMap<String, Integer>();
+            var themeHomeworks = new HashMap<String, Integer>();
+            var exercises = mapExercises.get(nameTheme.getKey());
+            for (var exercise : exercises.entrySet()) {
+                themeExercises.put(exercise.getKey(), Integer.parseInt(data[exercise.getValue()]));
+            }
+            var homeworks = mapHomeworks.get(nameTheme.getKey());
+            for (var homework : homeworks.entrySet()) {
+                themeHomeworks.put(homework.getKey(), Integer.parseInt(data[homework.getValue()]));
+            }
+
+            var theme = new Theme(themeExercises, themeHomeworks,
+                    Integer.parseInt(data[nameTheme.getValue()]),
+                    Integer.parseInt(data[mapSeminar.get(nameTheme.getKey())]), nameTheme.getKey());
+            student.addTheme(theme);
+        }
+        return student;
+    }
+
+    public StudentStorage createStudentStorage() {
+        var studentStorage = new StudentStorage();
+        for (var i = 3; i < table.size(); i++) {
+            studentStorage.addStudent(createStudent(table.get(i)));
+        }
+        return studentStorage;
+    }
+}
